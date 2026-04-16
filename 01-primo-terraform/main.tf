5664239fce01aead406d58c5835d53591c20ba8d
Domenico <domenico@192.168.1.62>
2026-04-16 12:02:06 +0200

    feat: static website hosting on S3 with public bucket policy

--- 01-primo-terraform/main.tf
+++ 01-primo-terraform/main.tf
@@ -15,7 +15,24 @@ resource "aws_s3_bucket" "primo_bucket" {
     bucket = "domenico-cloud-lab-2026"
 
     tags = {
-        Name = "Il mio primo bucket"
+        Name = "Sito statico"
         Environment = "Lab"
     }
+}
+
+resource "aws_s3_bucket_website_configuration" "sito" {
+    bucket = aws_s3_bucket.primo_bucket.id
+
+    index_document {
+        suffix = "index.html"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "sito" {
+    bucket = aws_s3_bucket.primo_bucket.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
 }
