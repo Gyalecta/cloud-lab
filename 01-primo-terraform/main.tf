d9aadf4bf8a85e6aa825b7db63fe2404bc55b274
Domenico <domenico@192.168.1.62>
2026-04-17 12:01:49 +0200

    feat: add CloudFront CDN with HTTPS in front of S3

--- 01-primo-terraform/main.tf
+++ 01-primo-terraform/main.tf
@@ -1,38 +1,89 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
-resource "aws_s3_bucket" "primo_bucket" {
-    bucket = "domenico-cloud-lab-2026"
+resource "aws_s3_bucket" "sito" {
+  bucket        = "domenico-cloud-lab-2026"
+  force_destroy = true
 
-    tags = {
-        Name = "Sito statico"
-        Environment = "Lab"
-    }
+  tags = {
+    Name        = "Sito statico"
+    Environment = "Lab"
+  }
 }
 
 resource "aws_s3_bucket_website_configuration" "sito" {
-    bucket = aws_s3_bucket.primo_bucket.id
+  bucket = aws_s3_bucket.sito.id
 
-    index_document {
-        suffix = "index.html"
-    }
+  index_document {
+    suffix = "index.html"
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "sito" {
-    bucket = aws_s3_bucket.primo_bucket.id
+  bucket = aws_s3_bucket.sito.id
 
-    block_public_acls       = false
-    block_public_policy     = false
-    ignore_public_acls      = false
-    restrict_public_buckets = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_cloudfront_distribution" "sito" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.sito.website_endpoint
+    origin_id   = "S3-domenico-cloud-lab"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-domenico-cloud-lab"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name        = "Sito statico CDN"
+    Environment = "Lab"
+  }
+}
+
+output "cloudfront_url" {
+  value = "https://${aws_cloudfront_distribution.sito.domain_name}"
 }
